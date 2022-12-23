dd2eb11d56966ac605b0e22f32abe20a861213bd
Stefano Balocco <stefano.balocco@gmail.com>
2022-12-23 13:17:31 +0100

    Simple code refactor Version bump

--- Router.php
+++ Router.php
@@ -31,14 +31,10 @@
 
 namespace StefanoBalocco\DamnSmallRouter;
 
-if( !function_exists( 'http_response_code' ) )
-{
-	function http_response_code( $code = NULL )
-	{
-		if( ( NULL !== $code ) && is_numeric( $code ) )
-		{
-			switch( $code )
-			{
+if( !function_exists( 'http_response_code' ) ) {
+	function http_response_code( $code = NULL ) {
+		if( ( NULL !== $code ) && is_numeric( $code ) ) {
+			switch( $code ) {
 				case 100: $text = 'Continue'; break;
 				case 101: $text = 'Switching Protocols'; break;
 				case 200: $text = 'OK'; break;
@@ -76,8 +72,7 @@ if( !function_exists( 'http_response_code' ) )
 				case 503: $text = 'Service Unavailable'; break;
 				case 504: $text = 'Gateway Time-out'; break;
 				case 505: $text = 'HTTP Version not supported'; break;
-				default:
-				{
+				default: {
 					exit('Unknown http status code "' . htmlentities($code) . '"');
 					break;
 				}
@@ -85,17 +80,14 @@ if( !function_exists( 'http_response_code' ) )
 			$protocol = ( isset( $_SERVER[ 'SERVER_PROTOCOL' ] ) ? $_SERVER[ 'SERVER_PROTOCOL' ] : 'HTTP/1.0' );
 			header( $protocol . ' ' . $code . ' ' . $text );
 			$GLOBALS[ 'http_response_code' ] = $code;
-		}
-		else
-		{
+		} else {
 			$code = ( isset( $GLOBALS[ 'http_response_code' ] ) ? $GLOBALS[ 'http_response_code' ] : 200 );
 		}
-		return( $code );
+		return $code;
 	}
 }
 
-class Router
-{
+class Router {
 	private static $instance;
 
 	protected $routes = array( );
@@ -104,195 +96,144 @@ class Router
 	protected $route405 = array( null, null );
 	protected $route500 = array( null, null );
 
-	private function __construct( )
-	{
+	private function __construct( ) { }
 
-	}
-
-	private static function CalculateWeight( $route )
-	{
+	private static function CalculateWeight( $route ) {
 		$returnValue = null;
 		$route = preg_replace( '/[\w]+/', '', $route );
 		$last = -1;
-		while( false !== ( $last = strpos( $route, '@', $last + 1 ) ) )
-		{
+		while( false !== ( $last = strpos( $route, '@', $last + 1 ) ) ) {
 			$returnValue = dechex( $last + 1 ) . $returnValue;
-			if( 0 == strlen( $returnValue ) )
-			{
+			if( 0 == strlen( $returnValue ) ) {
 				$returnValue = '00';
-			}
-			elseif( ( 1 == ( strlen( $returnValue ) % 2 ) ) )
-			{
+			} elseif( ( 1 == ( strlen( $returnValue ) % 2 ) ) ) {
 				$returnValue = '0' . $returnValue;
 			}
 		}
-		return( strlen( $returnValue ) ? $returnValue : '00' );
+		return ( strlen( $returnValue ) ? $returnValue : '00' );
 	}
 
-	public static function GetInstance( )
-	{
-		if( !self::$instance )
-		{
+	public static function GetInstance( ) {
+		if( !self::$instance ) {
 			self::$instance = new self( );
-			self::$instance->AddRoute403
-			(
-				function( )
-				{
+			self::$instance->AddRoute403 (
+				function( ) {
 					http_response_code( 403 );
-					return( null );
+					return null;
 				},
 				array( )
 			);
-			self::$instance->AddRoute404
-			(
-				function( )
-				{
+			self::$instance->AddRoute404 (
+				function( ) {
 					http_response_code( 404 );
-					return( null );
+					return null;
 				},
 				array( )
 			);
-			self::$instance->AddRoute405
-			(
-				function( )
-				{
+			self::$instance->AddRoute405 (
+				function( ) {
 					http_response_code( 405 );
-					return( null );
+					return null;
 				},
 				array( )
 			);
-			self::$instance->AddRoute500
-			(
-				function( )
-				{
+			self::$instance->AddRoute500(
+				function( ) {
 					http_response_code( 500 );
-					return( null );
+					return null;
 				},
 				array( )
 			);
 		}
-		return( self::$instance );
+		return self::$instance;
 	}
 
-	public static function AddRoute403( $callback, $variables = array( ) )
-	{
+	public static function AddRoute403( $callback, $variables = array( ) ) {
 		self::GetInstance( )->route403 = array( $callback, $variables );
 	}
 
-	public static function AddRoute404( $callback, $variables = array( ) )
-	{
+	public static function AddRoute404( $callback, $variables = array( ) ) {
 		self::GetInstance( )->route404 = array( $callback, $variables );
 	}
 
-	public static function AddRoute405( $callback, $variables = array( ) )
-	{
+	public static function AddRoute405( $callback, $variables = array( ) ) {
 		self::GetInstance( )->route405 = array( $callback, $variables );
 	}
 
-	public static function AddRoute500( $callback, $variables = array( ) )
-	{
+	public static function AddRoute500( $callback, $variables = array( ) ) {
 		self::GetInstance( )->route500 = array( $callback, $variables );
 	}
 
-	public static function AddRoute( $route, $callback, $variables = array( ), $method = 'GET', $available = null )
-	{
+	public static function AddRoute( $route, $callback, $variables = array( ), $method = 'GET', $available = null ) {
 		$routes = &self::GetInstance( )->routes;
 		$id = md5( $route );
-		if( !array_key_exists( $id, $routes ) )
-		{
-			$delimiter = '#';
-			if( PHP_VERSION_ID >= 70300) {
-				$delimiter = '\\#';
-			}
-			$routes[ $id ] = array
-			(
-				'regex' => '@^' . str_replace( array( $delimiter . 'AZ09' . $delimiter, $delimiter . 'AZ' . $delimiter, $delimiter . '09' . $delimiter), array( '([\w,]+)', '([a-zA-Z]+)', '(\d+)' ), preg_quote( $route ) ) . '$@',
+		if( !array_key_exists( $id, $routes ) ) {
+			$routes[ $id ] = array (
+				'regex' => '@^' . str_replace( array( '#AZ09#', '#AZ#', '#09#' ), array( '([\w,]+)', '([a-zA-Z]+)', '(\d+)' ), preg_quote( $route ) ) . '$@',
 				'weight' => self::CalculateWeight( $route ),
 				'methods' => array( )
 			);
 		}
-		if( !isset( $routes[ $id ][ 'methods' ][ $method ] ) || !$routes[ $id ][ 'methods' ][ $method ][ 2 ] )
-		{
+		if( !isset( $routes[ $id ][ 'methods' ][ $method ] ) || !$routes[ $id ][ 'methods' ][ $method ][ 2 ] ) {
 			$routes[ $id ][ 'methods' ][ $method ] = array( $callback, $variables, $available );
 		}
 	}
 
-	public static function RouteAvailable( $method = 'GET', $withoutConditions = false )
-	{
+	public static function RouteAvailable( $method = 'GET', $withoutConditions = false ) {
 		$returnValue = false;
 		$routes = self::GetInstance( )->routes;
 		$callback = array( null, null, null );
-		foreach( $routes as $route )
-		{
+		foreach( $routes as $route ) {
 			$matches = array( );
-			if( preg_match( $route[ 'regex' ], ( isset( $_SERVER[ 'PATH_INFO' ] ) ? $_SERVER[ 'PATH_INFO' ] : '' ), $matches ) && ( is_null( $callback[ 2 ] ) || ( $callback[ 2 ] > $route[ 'weight' ] ) ) )
-			{
-				if( array_key_exists( $method, $route[ 'methods' ] ) )
-				{
-					if( is_null( $route[ 'methods' ][ $method ][ 2 ] ) || ( !$withoutConditions && $route[ 'methods' ][ $method ][ 2 ] ) )
-					{
-						if( is_callable( $route[ 'methods' ][ $method ][ 0 ] ) )
-						{
+			if( preg_match( $route[ 'regex' ], ( isset( $_SERVER[ 'PATH_INFO' ] ) ? $_SERVER[ 'PATH_INFO' ] : '' ), $matches ) && ( is_null( $callback[ 2 ] ) || ( $callback[ 2 ] > $route[ 'weight' ] ) ) ) {
+				if( array_key_exists( $method, $route[ 'methods' ] ) ) {
+					if( is_null( $route[ 'methods' ][ $method ][ 2 ] ) || ( !$withoutConditions && $route[ 'methods' ][ $method ][ 2 ] ) ) {
+						if( is_callable( $route[ 'methods' ][ $method ][ 0 ] ) ) {
 							$returnValue = true;
 						}
 					}
 				}
 			}
 		}
-		return( $returnValue );
+		return $returnValue;
 	}
 
-	public static function Route( )
-	{
+	public static function Route( ) {
 		$returnValue = null;
 		$routes = self::GetInstance( )->routes;
 		$method = ( ( 'HEAD' == $_SERVER[ 'REQUEST_METHOD' ] ) ? 'GET' : $_SERVER[ 'REQUEST_METHOD' ] );
 		$callback = array( null, null, null );
-		foreach( $routes as $route )
-		{
+		foreach( $routes as $route ) {
 			$matches = array( );
-			if( preg_match( $route[ 'regex' ], ( isset( $_SERVER[ 'PATH_INFO' ] ) ? $_SERVER[ 'PATH_INFO' ] : '' ), $matches ) && ( is_null( $callback[ 2 ] ) || ( $callback[ 2 ] > $route[ 'weight' ] ) ) )
-			{
-				if( array_key_exists( $method, $route[ 'methods' ] ) )
-				{
-					if( is_null( $route[ 'methods' ][ $method ][ 2 ] ) || $route[ 'methods' ][ $method ][ 2 ] )
-					{
-						if( is_callable( $route[ 'methods' ][ $method ][ 0 ] ) )
-						{
-							$callback = array
-							(
+			if( preg_match( $route[ 'regex' ], ( isset( $_SERVER[ 'PATH_INFO' ] ) ? $_SERVER[ 'PATH_INFO' ] : '' ), $matches ) && ( is_null( $callback[ 2 ] ) || ( $callback[ 2 ] > $route[ 'weight' ] ) ) ) {
+				if( array_key_exists( $method, $route[ 'methods' ] ) ) {
+					if( is_null( $route[ 'methods' ][ $method ][ 2 ] ) || $route[ 'methods' ][ $method ][ 2 ] ) {
+						if( is_callable( $route[ 'methods' ][ $method ][ 0 ] ) ) {
+							$callback = array(
 								$route[ 'methods' ][ $method ][ 0 ],
 								array_merge( $route[ 'methods' ][ $method ][ 1 ], array_splice( $matches, 1 ) ),
 								$route[ 'weight' ]
 							);
-						}
-						else
-						{
+						} else {
 							$callback = self::GetInstance( )->route500;
 							$callback[ 2 ] = $route[ 'weight' ];
 						}
-					}
-					else
-					{
+					} else {
 						$callback = self::GetInstance( )->route403;
 						$callback[ 2 ] = $route[ 'weight' ];
 					}
-				}
-				else
-				{
+				} else {
 					$callback = self::GetInstance( )->route405;
 					$callback[ 2 ] = $route[ 'weight' ];
 				}
 			}
 		}
-		if( is_null( $callback[ 0 ] ) )
-		{
+		if( is_null( $callback[ 0 ] ) ) {
 			$callback = self::GetInstance( )->route404;
 		}
-		if( !is_null( $callback[ 0 ] ) )
-		{
+		if( !is_null( $callback[ 0 ] ) ) {
 			$returnValue = call_user_func_array( $callback[ 0 ], $callback[ 1 ] );
 		}
-		return( $returnValue );
+		return $returnValue;
 	}
 }
